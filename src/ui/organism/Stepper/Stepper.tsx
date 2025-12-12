1e1313662189e65e34cb1cffce0d4b5a675e1154
Kochkina Ekaterina <Gudrum1983@gmail.com>
2025-12-12 19:47:22 +0100

    Refactor (#10)
    * Рефакторинг + Readme generate-react-cli
    
    * Рефакторинг + Readme ui
    
    * Рефакторинг изменение структуры файлов стилей
    
    * Рефакторинг изменение структуры файлов стилей + создание Logo
    
    * Рефакторинг стилей и упрощение компонентов Logo, Header, Board и Game
    
    * Вынос HistoryStepItem в отдельный компонент

--- src/ui/organism/Stepper/Stepper.tsx
+++ src/ui/organism/Stepper/Stepper.tsx
@@ -1,7 +1,7 @@
 import styles from './Stepper.module.scss';
-import { Button, Typography } from '../../atoms';
-import clsx from 'clsx';
+import { Button } from '../../atoms';
 import type { HistoryItem } from '@types';
+import { HistoryStepItem } from '../../molecules';
 
 type StepperProps = {
   isAscending: boolean,
@@ -13,38 +13,27 @@ type StepperProps = {
 
 export function Stepper({ isAscending, setIsAscending, sortedMoves, currentMove, setCurrentMove }: StepperProps) {
 
-  const steps = sortedMoves.map((squares) => {
-
-    const step = squares.step;
-    const isStartStep = (squares.step === 0);
-    const isCurrentStep = (squares.step === currentMove);
-    const coordinatesText = squares.coordinates ? '(' + squares.coordinates[0] + ',' + squares.coordinates[1] + ')' : '';
-    const currentPlayerText = squares.currentPlayer ? squares.currentPlayer : '';
-
-    const baseDescription = isStartStep
-      ? 'К началу игры'
-      : `К шагу № ${step} -> ${currentPlayerText} : ${coordinatesText}`;
-
-    const currentDescription = isStartStep
-      ? 'Вы в начале игры'
-      : `Шаг № ${step} -> ${currentPlayerText} : ${coordinatesText}`;
-
-    return (
-      <li key={step} className={clsx(styles.Game_ListItem, (step === currentMove) && styles.ListItem_accent)}>
-        {(isCurrentStep)
-          ? <Typography variant={'accent'}>{currentDescription}</Typography>
-          : <Button onClick={() => setCurrentMove(step)} description={baseDescription} widthFull />
-        }
-      </li>
-    );
-  });
-
   return (
     <div className={styles.Stepper}>
-      <Button description={isAscending ? 'Шаги по возрастанию' : 'Шаги по убыванию'} widthFull isAccent textCenter
-              onClick={() => setIsAscending(!isAscending)} />
+      <Button
+        widthFull
+        isAccent
+        textCenter
+        description={isAscending ? 'Шаги по возрастанию' : 'Шаги по убыванию'}
+        onClick={() => setIsAscending(!isAscending)}
+      />
+
       <div className={styles.History}>
-        <ul className={styles.Game_List}>{steps}</ul>
+        <ul className={styles.Game_List}>
+          {sortedMoves.map((item) => (
+            <HistoryStepItem
+              key={item.step}
+              item={item}
+              isCurrent={item.step === currentMove}
+              onSelect={setCurrentMove}
+            />
+          ))}
+        </ul>
       </div>
     </div>
   );
